c86e169b21babb9cde7e0730808bfaae0b9eba2f
Jon Kerr <kerrjon@yahoo.com>
2016-04-03 23:30:44 -0500

    adding initial models and views

--- MnCalculator/MnCalculator/Models/CalculatorResult.cs
+++ MnCalculator/MnCalculator/Models/CalculatorResult.cs
@@ -1,18 +1,95 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace MnCalculator.Models
 {
     public class CalculatorResult
     {
-        public string Result { get; set; }
+        public Parent ParentA { get; set; }
+        public Parent ParentB { get; set; }
+        public Parent Combined { get; set; }
+        [Display(Name = "Combined Basic Support Obligation")]
+        public decimal CombinedBasicSupportObligation { get; set; }
+        [Display(Name = "Number of Children")]
+        public int NumberOfChildren { get; set; }
+        [Display(Name = "IV-D Case Number")]
+        public string CaseNumber { get; set; }
+        [Display(Name = "Court File Number")]
+        public string CourtFileNumber { get; set; }
 
         public static CalculatorResult Create(ChildSupportForm input)
         {
-            var model = new CalculatorResult() {Result = "test" };
+            var combinedSupportObligation = 2777;
+            var parentBOvernights = 365 - input.ParentAOvernights;
+            var model = new CalculatorResult
+            {
+                ParentA = new Parent
+                {
+                    Name = input.ParentA,
+                    GrossIncome = decimal.Round(input.ParentAGrossIncome, 0),
+                    PercentageOfParentingTime = decimal.Round(input.ParentAOvernights/(input.ParentAOvernights + parentBOvernights) *100, 1),
+                    Overnights = decimal.Round(input.ParentAOvernights, 1),
+                    PicsAmount = input.ParentAGrossIncome, //todo: subtract other child
+                    PicsPercentage = decimal.Round(input.ParentAGrossIncome / (input.ParentAGrossIncome + input.ParentBGrossIncome),3) * 100,
+                    ProRataBasicSupportObligation = decimal.Round(input.ParentAGrossIncome / (input.ParentAGrossIncome + input.ParentBGrossIncome) * combinedSupportObligation,2)
+                },
+                ParentB = new Parent
+                {
+                    Name = input.ParentB,
+                    GrossIncome = decimal.Round(input.ParentBGrossIncome, 0),
+                    PercentageOfParentingTime = decimal.Round(parentBOvernights / (input.ParentAOvernights + parentBOvernights) *100, 1),
+                    Overnights = decimal.Round(input.ParentAOvernights, 1),
+                    PicsAmount = input.ParentBGrossIncome, //todo: subtract other child
+                    PicsPercentage = decimal.Round(input.ParentBGrossIncome / (input.ParentAGrossIncome + input.ParentBGrossIncome),3) * 100,
+                    ProRataBasicSupportObligation = decimal.Round(input.ParentBGrossIncome / (input.ParentAGrossIncome + input.ParentBGrossIncome) * combinedSupportObligation, 2)
+                },
+                Combined = new Parent
+                {
+                    Name = string.Empty,
+                    GrossIncome = decimal.Round(input.ParentAGrossIncome + input.ParentBGrossIncome, 0),
+                    PercentageOfParentingTime = 100,
+                    Overnights = input.ParentAOvernights + parentBOvernights,
+                    PicsAmount = input.ParentAGrossIncome + input.ParentBGrossIncome, //todo: subtract other child
+                    PicsPercentage = 100,
+                },
+                CombinedBasicSupportObligation = combinedSupportObligation,  //ToDo: get from xml
+                NumberOfChildren = input.NumberOfChildren,
+                CourtFileNumber = input.CourtFileNumber,
+                CaseNumber = input.CaseNumber
+
+
+            };
+
+           
+
+
             return model;
         }
+    }
+
+    public class Parent
+    {
+        public string Name { get; set; }
+        public decimal Overnights { get; set; }
+
+        [Display(Name="Gross Income")]
+        public decimal GrossIncome { get; set; }
+
+        [Display(Name = "Percentage of Parenting Time")]
+        public decimal PercentageOfParentingTime { get; set; }
+
+        [Display(Name = "Percentage Share of Combined PICS")]
+        public decimal PicsPercentage { get; set; }
+
+        [Display(Name = "Parental Income for Determining Child Support")]
+        public decimal PicsAmount { get; set; }
+
+        [Display(Name = "Pro Rata Basic Support Obligation")]
+        public decimal ProRataBasicSupportObligation { get; set; }
+
+        [Display(Name = "Adjustment for Parenting Time")]
+        public decimal AdjustmentForParentingTime { get; set; }
+
+        [Display(Name = "Obligation After Parenting Time Adjustment")]
+        public decimal BasicSupportObligationAfterAdjustment { get; set; }
     }
 }
--- MnCalculator/MnCalculator/Models/ChildSupportForm.cs
+++ MnCalculator/MnCalculator/Models/ChildSupportForm.cs
@@ -4,26 +4,47 @@ namespace MnCalculator.Models
 {
     public class ChildSupportForm
     {
-        [Display(Name ="Parent A Gross Income")]
+        public ChildSupportForm()
+        {
+            NumberOfChildren = 1;
+            ParentAOvernights = (decimal) 182.5;
+        }
+
+        [Display(Name ="Parent A Name")]
         public string ParentA { get; set; }
 
-        [Display(Name = "Parent B Gross Income")]
+        [Display(Name = "Parent B Name")]
         public string ParentB { get; set; }
+
+        [Display(Name = "IV-D Case Number")]
+        public string CaseNumber { get; set; }
+
+        [Display(Name = "Court File Number")]
+        public string CourtFileNumber { get; set; }
 
         [Display(Name = "Enter the Number of Children")]
         [Required(ErrorMessage = "Number of Children is Required")]
         [Range(typeof(int), "1", "6", ErrorMessage = "Number of Children must be greater than 0 and less 7.  Enter 6 if you have 6 or more children.")]
         public int NumberOfChildren { get; set; }
 
-        [Display(Name = "Parent A Gross Income")]
+        [Display(Name = "Gross Income")]
         [Required(ErrorMessage = "Parent A Gross Income is Required")]
         [Range(typeof(decimal), "0", "79228162514264337593543950335", ErrorMessage="Gross income must be 0 or greater.  Enter zero for no income.")]
         public decimal ParentAGrossIncome { get; set; }
 
-        [Display(Name = "Parent B Gross Income")]
+        [Display(Name = "Gross Income")]
         [Required(ErrorMessage = "Parent B Gross Income is Required")]
         [Range(typeof(decimal), "0", "79228162514264337593543950335", ErrorMessage = "Gross income must be 0 or greater.  Enter zero for no income.")]
         public decimal ParentBGrossIncome { get; set; }
+
+        [Display(Name = "Number of overnights")]
+        [Required(ErrorMessage = "Parent A overnights is required")]
+        [Range(typeof(decimal), "0", "182.5", ErrorMessage = "Parent A is the parent with the least number of overnights.  Value must be between 0 and 182.5")]
+        public decimal ParentAOvernights { get; set; }
+
+        [Display(Name = "Number of overnights")]
+        [Range(typeof(decimal), "0", "365", ErrorMessage = "Number of overnights must be between 0 and 365.")]
+        public decimal ParentBOvernights { get; set; }
 
     }
 }
