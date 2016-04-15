344a2bb49d97d5aaa76b5b1ab3991c82b6ecdb33
Jon Kerr <jkerr@markarch.com>
2016-04-15 15:33:08 -0500

    adding gross income calculation

--- MnCalculator/MnCalculator/Models/CalculatorResult.cs
+++ MnCalculator/MnCalculator/Models/CalculatorResult.cs
@@ -8,153 +8,194 @@ using System.Xml.Serialization;
 
 namespace MnCalculator.Models
 {
-    public class CalculatorResult
-    {
-        public Parent ParentA { get; set; }
-        public Parent ParentB { get; set; }
-        public Parent Combined { get; set; }
-        [Display(Name = "Combined Basic Support Obligation")]
-        public decimal CombinedBasicSupportObligation { get; set; }
-        [Display(Name = "Number of Children")]
-        public int NumberOfChildren { get; set; }
-        [Display(Name = "IV-D Case Number")]
-        public string CaseNumber { get; set; }
-        [Display(Name = "Court File Number")]
-        public string CourtFileNumber { get; set; }
+  public class CalculatorResult
+  {
+    public Parent ParentA { get; set; }
+    public Parent ParentB { get; set; }
+    public Parent Combined { get; set; }
+    [Display(Name = "Combined Basic Support Obligation")]
+    public decimal CombinedBasicSupportObligation { get; set; }
+    [Display(Name = "Number of Children")]
+    public int NumberOfChildren { get; set; }
+    [Display(Name = "IV-D Case Number")]
+    public string CaseNumber { get; set; }
+    [Display(Name = "Court File Number")]
+    public string CourtFileNumber { get; set; }
 
-        public static CalculatorResult Create(ChildSupportForm input)
+    public static CalculatorResult Create(ChildSupportForm input)
+    {
+      var parentBOvernights = Math.Round(365 - input.ParentAOvernights, 1);
+
+      if (input.ParentASocialSecurityBenefits == null){input.ParentASocialSecurityBenefits = 0;}
+      if (input.ParentBSocialSecurityBenefits == null){input.ParentBSocialSecurityBenefits = 0;}
+      if (input.ParentASpousalMaintenance == null) { input.ParentASpousalMaintenance = 0; }
+      if (input.ParentBSpousalMaintenance == null) { input.ParentBSpousalMaintenance = 0; }
+      if (input.ParentAPotentialIncome == null) { input.ParentAPotentialIncome = 0; }
+      if (input.ParentBPotentialIncome == null) { input.ParentBPotentialIncome = 0; }
+      if (input.ParentANonJointChildSupport == null) { input.ParentANonJointChildSupport = 0; }
+      if (input.ParentBNonJointChildSupport == null) { input.ParentBNonJointChildSupport = 0; }
+
+
+      var parentAGrossIncome = input.ParentAMonthlyIncomeReceived + input.ParentASocialSecurityBenefits +
+                               input.ParentAPotentialIncome - input.ParentASpousalMaintenance -
+                               input.ParentANonJointChildSupport;
+      var parentBGrossIncome = input.ParentBMonthlyIncomeReceived + input.ParentBSocialSecurityBenefits +
+                               input.ParentBPotentialIncome - input.ParentBSpousalMaintenance -
+                               input.ParentBNonJointChildSupport;
+
+      var model = new CalculatorResult
+      {
+        ParentA = new Parent
         {
-
-            var parentBOvernights = Math.Round(365 - input.ParentAOvernights, 1);
-
-            var model = new CalculatorResult
-            {
-                ParentA = new Parent
-                {
-                    Name = input.ParentA,
-                    GrossIncome = (double)Math.Round(input.ParentAGrossIncome, 0),
-                    PercentageOfParentingTime =
+          Name = input.ParentA,
+          MonthlyIncomeReceived = (double)input.ParentAMonthlyIncomeReceived,
+          SocialSecurityBenefits = (double)input.ParentASocialSecurityBenefits,
+          PotenialIncome = (double)input.ParentAPotentialIncome,
+          SpousalMaintenance = (double)input.ParentASpousalMaintenance,
+          NonJointChildSupport = (double)input.ParentANonJointChildSupport,
+          GrossIncome = Math.Round((double) parentAGrossIncome, 0),
+          PercentageOfParentingTime =
                         Math.Round(input.ParentAOvernights / (input.ParentAOvernights + parentBOvernights), 3) * 100,
-                    Overnights = Math.Round(input.ParentAOvernights, 1),
-                    PicsAmount = (double)input.ParentAGrossIncome, //todo: subtract other child
-                    PicsPercentage =
+          Overnights = Math.Round(input.ParentAOvernights, 1),
+          PicsAmount = (double)input.ParentAMonthlyIncomeReceived, //todo: subtract other child
+          PicsPercentage =
                         (double)
-                            (Math.Round(input.ParentAGrossIncome / (input.ParentAGrossIncome + input.ParentBGrossIncome),
+                            (Math.Round(input.ParentAMonthlyIncomeReceived / (input.ParentAMonthlyIncomeReceived + input.ParentBMonthlyIncomeReceived),
                                 3) * 100)
-                },
-                ParentB = new Parent
-                {
-                    Name = input.ParentB,
-                    GrossIncome = (double)Math.Round(input.ParentBGrossIncome, 0),
-                    PercentageOfParentingTime =
+        },
+        ParentB = new Parent
+        {
+          Name = input.ParentB,
+          MonthlyIncomeReceived = (double)input.ParentBMonthlyIncomeReceived,
+          SocialSecurityBenefits = (double)input.ParentBSocialSecurityBenefits,
+          PotenialIncome = (double)input.ParentBPotentialIncome,
+          SpousalMaintenance = (double)input.ParentBSpousalMaintenance,
+          NonJointChildSupport = (double)input.ParentBNonJointChildSupport,
+          GrossIncome = Math.Round((double) parentBGrossIncome, 0),
+          PercentageOfParentingTime =
                         Math.Round(parentBOvernights / (input.ParentAOvernights + parentBOvernights), 3) * 100,
-                    Overnights = parentBOvernights,
-                    PicsAmount = (double)input.ParentBGrossIncome, //todo: subtract other child
-                    PicsPercentage =
+          Overnights = parentBOvernights,
+          PicsAmount = (double)input.ParentBMonthlyIncomeReceived, //todo: subtract other child
+          PicsPercentage =
                         (double)
-                            (Math.Round(input.ParentBGrossIncome / (input.ParentAGrossIncome + input.ParentBGrossIncome),
+                            (Math.Round(input.ParentBMonthlyIncomeReceived / (input.ParentAMonthlyIncomeReceived + input.ParentBMonthlyIncomeReceived),
                                 3) * 100)
-                },
-                Combined = new Parent
-                {
-                    Name = string.Empty,
-                    GrossIncome = (double)Math.Round(input.ParentAGrossIncome + input.ParentBGrossIncome, 0),
-                    PercentageOfParentingTime = 100,
-                    Overnights = input.ParentAOvernights + parentBOvernights,
-                    PicsAmount = (double)(input.ParentAGrossIncome + input.ParentBGrossIncome),
-                    //todo: subtract other child
-                    PicsPercentage = 100
-                },
-                NumberOfChildren = input.NumberOfChildren,
-                CourtFileNumber = input.CourtFileNumber,
-                CaseNumber = input.CaseNumber
-            };
+        },
+        Combined = new Parent
+        {
+          Name = string.Empty,
+          GrossIncome = (double)Math.Round(input.ParentAMonthlyIncomeReceived + input.ParentBMonthlyIncomeReceived, 0),
+          PercentageOfParentingTime = 100,
+          Overnights = input.ParentAOvernights + parentBOvernights,
+          PicsAmount = (double)(input.ParentAMonthlyIncomeReceived + input.ParentBMonthlyIncomeReceived),
+          //todo: subtract other child
+          PicsPercentage = 100
+        },
+        NumberOfChildren = input.NumberOfChildren,
+        CourtFileNumber = input.CourtFileNumber,
+        CaseNumber = input.CaseNumber
+      };
 
-            //lookup the obligation amount from XML
-            var serializer = new XmlSerializer(typeof(Obligation));
-            using (var reader = XmlReader.Create(HttpContext.Current.Server.MapPath("~/SupportObligation.xml")))
-            {
-                var obligationData = (Obligation)serializer.Deserialize(reader);
-                model.CombinedBasicSupportObligation = obligationData.Incomes.Where(i => model.Combined.PicsAmount >= i.Min && model.Combined.PicsAmount <= i.Max).SelectMany(i => i.Amounts).First(x => x.Children == input.NumberOfChildren).Value;
-            }
+      //lookup the obligation amount from XML
+      var serializer = new XmlSerializer(typeof(Obligation));
+      using (var reader = XmlReader.Create(HttpContext.Current.Server.MapPath("~/SupportObligation.xml")))
+      {
+        var obligationData = (Obligation)serializer.Deserialize(reader);
+        model.CombinedBasicSupportObligation = obligationData.Incomes.Where(i => model.Combined.PicsAmount >= i.Min && model.Combined.PicsAmount <= i.Max).SelectMany(i => i.Amounts).First(x => x.Children == input.NumberOfChildren).Value;
+      }
 
-            model.ParentA.ProRataBasicSupportObligation = (double)Math.Round(input.ParentAGrossIncome / (input.ParentAGrossIncome + input.ParentBGrossIncome) * model.CombinedBasicSupportObligation, 2);
-            model.ParentB.ProRataBasicSupportObligation = (double)Math.Round(input.ParentBGrossIncome / (input.ParentAGrossIncome + input.ParentBGrossIncome) * model.CombinedBasicSupportObligation, 2);
+      model.ParentA.ProRataBasicSupportObligation = (double)Math.Round(input.ParentAMonthlyIncomeReceived / (input.ParentAMonthlyIncomeReceived + input.ParentBMonthlyIncomeReceived) * model.CombinedBasicSupportObligation, 2);
+      model.ParentB.ProRataBasicSupportObligation = (double)Math.Round(input.ParentBMonthlyIncomeReceived / (input.ParentAMonthlyIncomeReceived + input.ParentBMonthlyIncomeReceived) * model.CombinedBasicSupportObligation, 2);
 
-            //calculate the parenting time adjustment
-            var parentingTimeAdjusment = ((Math.Pow(parentBOvernights, 3.00) * model.ParentA.ProRataBasicSupportObligation) -
-                                    (Math.Pow(input.ParentAOvernights, 3.00) * model.ParentB.ProRataBasicSupportObligation)) /
-                                   (Math.Pow(parentBOvernights, 3.00) + (Math.Pow(input.ParentAOvernights, 3.00)));
+      //calculate the parenting time adjustment
+      var parentingTimeAdjusment = ((Math.Pow(parentBOvernights, 3.00) * model.ParentA.ProRataBasicSupportObligation) -
+                              (Math.Pow(input.ParentAOvernights, 3.00) * model.ParentB.ProRataBasicSupportObligation)) /
+                             (Math.Pow(parentBOvernights, 3.00) + (Math.Pow(input.ParentAOvernights, 3.00)));
 
-            if (parentingTimeAdjusment < 0) //Parent B is obligor (it is not A as described in bill?)
-            {
-                model.ParentA.BasicSupportObligationAfterAdjustment = 0;
-                model.ParentB.BasicSupportObligationAfterAdjustment = Math.Round(parentingTimeAdjusment * -1, 2);
-            }
-            else
-            {
-                model.ParentA.BasicSupportObligationAfterAdjustment = Math.Round(parentingTimeAdjusment, 2);
-                model.ParentB.BasicSupportObligationAfterAdjustment = 0;
-            }
+      if (parentingTimeAdjusment < 0) //Parent B is obligor (it is not A as described in bill?)
+      {
+        model.ParentA.BasicSupportObligationAfterAdjustment = 0;
+        model.ParentB.BasicSupportObligationAfterAdjustment = Math.Round(parentingTimeAdjusment * -1, 2);
+      }
+      else
+      {
+        model.ParentA.BasicSupportObligationAfterAdjustment = Math.Round(parentingTimeAdjusment, 2);
+        model.ParentB.BasicSupportObligationAfterAdjustment = 0;
+      }
 
-            return model;
-        }
+      return model;
     }
+  }
 
-    public class Parent
-    {
-        public string Name { get; set; }
-        public double Overnights { get; set; }
+  public class Parent
+  {
+    public string Name { get; set; }
+    public double Overnights { get; set; }
 
-        [Display(Name = "Gross Income")]
-        public double GrossIncome { get; set; }
+    [Display(Name = "1a. Monthly Income Received")]
+    public double MonthlyIncomeReceived { get; set; }
 
-        [Display(Name = "Percentage of Parenting Time")]
-        public double PercentageOfParentingTime { get; set; }
+    [Display(Name = "1b. Child(ren)'s Social Security/Veterans' Benefits Derived From a Parent's Eligibility")]
+    public double SocialSecurityBenefits { get; set; }
 
-        [Display(Name = "Percentage Share of Combined PICS")]
-        public double PicsPercentage { get; set; }
+    [Display(Name = "1c. Potential Income")]
+    public double PotenialIncome { get; set; }
 
-        [Display(Name = "Parental Income for Determining Child Support")]
-        public double PicsAmount { get; set; }
+    [Display(Name = "1d. Spousal Maintenance Orders Obligated to be Paid")]
+    public double SpousalMaintenance { get; set; }
 
-        [Display(Name = "Pro Rata Basic Support Obligation")]
-        public double ProRataBasicSupportObligation { get; set; }
+    [Display(Name = "1e. Child Support Order(s) Obligated to be Paid for Nonjoint Child(ren)")]
+    public double NonJointChildSupport { get; set; }
 
-        [Display(Name = "Adjustment for Parenting Time")]
-        public double AdjustmentForParentingTime { get; set; }
+    [Display(Name = "1f. Monthly Gross Income (1a+1b+1c-1d-1e)")]
+    public double GrossIncome { get; set; }
 
-        [Display(Name = "Obligation After Parenting Time Adjustment")]
-        public double BasicSupportObligationAfterAdjustment { get; set; }
-    }
+    [Display(Name = "Percentage of Parenting Time")]
+    public double PercentageOfParentingTime { get; set; }
 
-    public class Income
-    {
-        [XmlAttribute("min")]
-        public int Min { get; set; }
+    [Display(Name = "Percentage Share of Combined PICS")]
+    public double PicsPercentage { get; set; }
 
-        [XmlAttribute("max")]
-        public int Max { get; set; }
+    [Display(Name = "Parental Income for Determining Child Support")]
+    public double PicsAmount { get; set; }
 
-        [XmlArray("amounts")]
-        [XmlArrayItem("amount")]
-        public List<Amount> Amounts { get; set; }
-    }
+    [Display(Name = "Pro Rata Basic Support Obligation")]
+    public double ProRataBasicSupportObligation { get; set; }
 
-    public class Amount
-    {
-        [XmlAttribute("children")]
-        public int Children { get; set; }
+    [Display(Name = "Adjustment for Parenting Time")]
+    public double AdjustmentForParentingTime { get; set; }
 
-        [XmlAttribute("value")]
-        public int Value { get; set; }
-    }
+    [Display(Name = "Obligation After Parenting Time Adjustment")]
+    public double BasicSupportObligationAfterAdjustment { get; set; }
+  }
 
-    [XmlRoot("obligation")]
-    public class Obligation
-    {
-        [XmlArray("incomes")]
-        [XmlArrayItem("income")]
-        public List<Income> Incomes { get; set; }
-    }
+  public class Income
+  {
+    [XmlAttribute("min")]
+    public int Min { get; set; }
+
+    [XmlAttribute("max")]
+    public int Max { get; set; }
+
+    [XmlArray("amounts")]
+    [XmlArrayItem("amount")]
+    public List<Amount> Amounts { get; set; }
+  }
+
+  public class Amount
+  {
+    [XmlAttribute("children")]
+    public int Children { get; set; }
+
+    [XmlAttribute("value")]
+    public int Value { get; set; }
+  }
+
+  [XmlRoot("obligation")]
+  public class Obligation
+  {
+    [XmlArray("incomes")]
+    [XmlArrayItem("income")]
+    public List<Income> Incomes { get; set; }
+  }
 }
--- MnCalculator/MnCalculator/Models/ChildSupportForm.cs
+++ MnCalculator/MnCalculator/Models/ChildSupportForm.cs
@@ -1,15 +1,17 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections;
+using System.ComponentModel.DataAnnotations;
+using System.EnterpriseServices;
 
 namespace MnCalculator.Models
 {
-    public class ChildSupportForm
+    public class ChildSupportForm : IEnumerable
     {
         public ChildSupportForm()
         {
             NumberOfChildren = 1;
-            ParentAGrossIncome = 2000;
+            ParentAMonthlyIncomeReceived = 2000;
             ParentAOvernights = 182.5;
-            ParentBGrossIncome = 2000;
+            ParentBMonthlyIncomeReceived = 2000;
         }
 
         [Display(Name ="Parent A Name")]
@@ -29,24 +31,72 @@ namespace MnCalculator.Models
         [Range(typeof(int), "1", "6", ErrorMessage = "Number of Children must be greater than 0 and less 7.  Enter 6 if you have 6 or more children.")]
         public int NumberOfChildren { get; set; }
 
-        [Display(Name = "Monthly Gross Income")]
-        [Required(ErrorMessage = "Parent A Gross Income is Required")]
-        [Range(typeof(decimal), "0", "79228162514264337593543950335", ErrorMessage="Gross income must be 0 or greater.  Enter zero for no income.")]
-        public decimal ParentAGrossIncome { get; set; }
+        [Display(Name = "What is the monthly income received?")]
+        [Required(ErrorMessage = "Parent A Monthly Income is Required")]
+        [Range(typeof(decimal), "0", "79228162514264337593543950335", ErrorMessage="Monthly income must be 0 or greater.  Enter 0 for no income.")]
+        public decimal ParentAMonthlyIncomeReceived { get; set; }
 
-        [Display(Name = "Monthly Gross Income")]
-        [Required(ErrorMessage = "Parent B Gross Income is Required")]
-        [Range(typeof(decimal), "0", "79228162514264337593543950335", ErrorMessage = "Gross income must be 0 or greater.  Enter zero for no income.")]
-        public decimal ParentBGrossIncome { get; set; }
+        [Display(Name = "What is the monthly income received?")]
+        [Required(ErrorMessage = "Parent B Monthly Income is Required")]
+        [Range(typeof(decimal), "0", "79228162514264337593543950335", ErrorMessage = "Monthly income must be 0 or greater.  Enter 0 for no income.")]
+        public decimal ParentBMonthlyIncomeReceived { get; set; }
 
-        [Display(Name = "Number of overnights")]
+        [Display(Name = "What is the number of the overnights for each parent?")]
         [Required(ErrorMessage = "Parent A overnights is required")]
         [Range(typeof(decimal), "0", "182.5", ErrorMessage = "Parent A is the parent with the least number of overnights.  Value must be between 0 and 182.5")]
         public double ParentAOvernights { get; set; }
 
-        [Display(Name = "Number of overnights")]
+        [Display(Name = "What is the number of the overnights for each parent?")]
         [Range(typeof(decimal), "0", "365", ErrorMessage = "Number of overnights must be between 0 and 365.")]
         public decimal ParentBOvernights { get; set; }
 
+        [Display(Name = "What is the potential income for each parent, if any?")]
+        [Range(typeof(decimal), "0", "79228162514264337593543950335", ErrorMessage = "Must be number greater than or equal to 0.")]
+        public decimal? ParentAPotentialIncome { get; set; }
+
+        [Display(Name = "What is the potential income for each parent, if any?")]
+        [Range(typeof(decimal), "0", "79228162514264337593543950335", ErrorMessage = "Must be number greater than or equal to 0.")]
+        public decimal? ParentBPotentialIncome { get; set; }
+
+        [Display(Name = "What is the monthly amount the joint child(ren) receive in benefits from Social Security or the U.S. Department of Veterans Affairs (VA) due to a parent's eligibility?")]
+        [Range(typeof(decimal), "0", "79228162514264337593543950335", ErrorMessage = "Must be number greater than or equal to 0.")]
+        public decimal? ParentASocialSecurityBenefits { get; set; }
+
+        [Display(Name = "What is the monthly amount the joint child(ren) receive in benefits from Social Security or the U.S. Department of Veterans Affairs (VA) due to a parent's eligibility?")]
+        [Range(typeof(decimal), "0", "79228162514264337593543950335", ErrorMessage = "Must be number greater than or equal to 0.")]
+        public decimal? ParentBSocialSecurityBenefits { get; set; }
+
+        [Display(Name = "If the joint child(ren) receive Social Security or VA benefits due to Parent A's eligibility, is Parent B the representative payee?")]
+        public bool IsParentBRepresentativePayee { get; set; }
+
+        [Display(Name = "What is the monthly amount each parent is ordered to pay for spousal maintenance?")]
+        [Range(typeof(decimal), "0", "79228162514264337593543950335", ErrorMessage = "Must be number greater than or equal to 0.")]
+        public decimal? ParentASpousalMaintenance { get; set; }
+
+        [Display(Name = "What is the monthly amount each parent is ordered to pay for spousal maintenance? ")]
+        [Range(typeof(decimal), "0", "79228162514264337593543950335", ErrorMessage = "Must be number greater than or equal to 0.")]
+        public decimal? ParentBSpousalMaintenance { get; set; }
+
+        [Display(Name = "What is the total amount each parent is ordered to pay for monthly child support for nonjoint child(ren)?")]
+        [Range(typeof(decimal), "0", "79228162514264337593543950335", ErrorMessage = "Must be number greater than or equal to 0.")]
+        public decimal? ParentANonJointChildSupport { get; set; }
+
+        [Display(Name = "What is the total amount each parent is ordered to pay for monthly child support for nonjoint child(ren)? ")]
+        [Range(typeof(decimal), "0", "79228162514264337593543950335", ErrorMessage = "Must be number greater than or equal to 0.")]
+        public decimal? ParentBNonJointChildSupport { get; set; }
+
+        [Display(Name = "What is the number of nonjoint child(ren) living in the home?")]
+        [Range(typeof(int), "0", "50", ErrorMessage = "Must be number less than 50.")]
+        public int ParentANonJointChildren { get; set; }
+
+        [Display(Name = "What is the number of nonjoint child(ren) living in the home?")]
+        [Range(typeof(int), "0", "50", ErrorMessage = "Must be number less than 50.")]
+        public decimal ParentBNonJointChildren { get; set; }
+
+
+      public IEnumerator GetEnumerator()
+      {
+        throw new System.NotImplementedException();
+      }
     }
 }
